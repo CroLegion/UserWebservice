b7066fb5a358c6d7bd526781abdb068cf059e22e
jacob <stilwell@iastate.edu>
2019-06-11 22:15:18 -0500

    added most methods, errors still

--- CRUD-users/src/main/java/controller/UserController.java
+++ CRUD-users/src/main/java/controller/UserController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,20 +27,24 @@ public class UserController {
 		return ResponseEntity.ok().body("A new user has been added by the name of"+
 		user.getFirstName()+" "+user.getLastName()+" with the id of "+id);
 	}
-	@PostMapping("/user")
+	@GetMapping("/user")
 	public ResponseEntity<?> list(@RequestBody User user){
-		int id = userservice.save(user);
+		List<User> listUsers = userservice.list();
+		return ResponseEntity.ok().body(listUsers);
 	}
-	@PostMapping("/user{id}")
+	@PutMapping("/user{id}")
 	public ResponseEntity<?> update(@PathVariable("id") int id, User user){
-		int id = userservice.save(user);
+		userservice.update(id, user);
+		return ResponseEntity.ok().body("The user has been updated.");
 	}
-	@PostMapping("/user{id}")
+	@DeleteMapping("/user{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") int id){
-		int id = userservice.save(user);
+		userservice.delete(id);
+		return ResponseEntity.ok().body("The user has been deleted");
 	}
-	@PostMapping("/user{id}")
+	@GetMapping("/user{id}")
 	public ResponseEntity<?> getUser(@PathVariable("id") int id){
-		int id = userservice.save(user);
+		User user = userservice.getUser(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
